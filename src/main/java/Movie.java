2dcba5cc52bacdc54a85b8732f2c81315efb4cc9
giyeon95 <giyeon15@gmail.com>
2022-06-01 23:47:24 +0900

    feat: push member down

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,53 +1,22 @@
-public class Movie {
+public abstract class Movie {
 
     public static final int CHILDRENS = 2;
     public static final int NEW_RELEASE = 1;
     public static final int REGULAR = 0;
 
-    private String title;
-    private int pricecode;
+    protected String title;
+    protected int pricecode;
 
     public Movie(String title, int priceCode) {
         this.title = title;
         pricecode = priceCode;
     }
 
-    public int getPriceCode() {
-        return pricecode;
-    }
-
-    public void setPriceCode(int arg) {
-        pricecode = arg;
-    }
-
     public String getTitle() {
         return title;
     }
 
-    double determineAmount(int daysRented) {
-        double rentalAmount = 0;
-        switch (pricecode) {
-            case REGULAR:
-                rentalAmount += 2;
-                if (daysRented > 2)
-                    rentalAmount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                rentalAmount += daysRented * 3;
-                break;
-            case CHILDRENS:
-                rentalAmount += 1.5;
-                if (daysRented > 3)
-                    rentalAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
-    }
+    abstract double determineAmount(int daysRented);
 
-    int determineFrequentRentalPoint(int daysRented) {
-        boolean bonusIsEarned = (pricecode == NEW_RELEASE && daysRented > 1);
-        if (bonusIsEarned)
-            return 2;
-        return 1;
-    }
+    abstract int determineFrequentRentalPoint(int daysRented);
 }
--- src/main/java/RegularMovie.java
+++ src/main/java/RegularMovie.java
@@ -1,6 +1,35 @@
 public class RegularMovie extends Movie {
 
-    public RegularMovie(String title, int priceCode) {
-        super(title, priceCode);
+    public RegularMovie(String title) {
+        super(title, REGULAR);
+    }
+
+    @Override
+    double determineAmount(int daysRented) {
+        double rentalAmount = 0;
+        switch (pricecode) {
+            case REGULAR:
+                rentalAmount += 2;
+                if (daysRented > 2)
+                    rentalAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                rentalAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                rentalAmount += 1.5;
+                if (daysRented > 3)
+                    rentalAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
+
+    @Override
+    int determineFrequentRentalPoint(int daysRented) {
+        boolean bonusIsEarned = (pricecode == NEW_RELEASE && daysRented > 1);
+        if (bonusIsEarned)
+            return 2;
+        return 1;
     }
 }
